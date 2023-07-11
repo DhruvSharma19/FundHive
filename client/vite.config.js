c2dc3f1ed211d408388acce23143ad34ee57b45f
Dhruv Sharma <dhruvamsh@gmail.com>
2023-07-11 08:38:58 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,16 @@ import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   define: {
     global:"globalThis",
     "process.env": {},
   },
+  build:{
+    rollupOptions:{
+      external:["@safe-globalThis/safe-core-sdk","@safe-globalThis/safe-ethers-adapters","@safe-globalThis/safe-ethers-lib"],
+    }
+  }
 });
